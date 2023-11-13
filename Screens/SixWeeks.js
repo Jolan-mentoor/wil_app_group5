261f73f742503a32fdeb6e5d62dfad2bdf5da294
Jolan-mentoor <ST10351108@vcconnect.edu.za>
2023-11-13 23:09:31 +0200

    added code

--- Screens/SixWeeks.js
+++ Screens/SixWeeks.js
@@ -4,34 +4,41 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Six-Week Courses</Text>
+      <Text style={styles.title}> Six-Week Courses</Text>
 
 
-      <Text>Child Minding </Text>
-      <Text>Cooking</Text>
-      <Text>Garden Maintanance</Text>
+      <Text style={styles.text}>Child Minding </Text>
+
+
+      <Text style={styles.text}>Cooking</Text>
+
+
+      <Text style={styles.text}>Garden Maintanance</Text>
+
+
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '5E280B',
     alignItems: 'center',
     justifyContent: 'center',
   },
     title: {
     fontSize: 36,
-    color: ' ',
+    color: 'CEA792',
     marginBottom: 24,
     textAlign: 'center',
   },
 
   text: {
     fontSize: 36,
-    color: ' ',
+    color: 'CEA792',
     marginBottom: 24,
     textAlign: 'center',
 
